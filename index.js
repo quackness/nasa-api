766a300a58fdbbc1d7ed9358679e36dd6349c6c0
Karolina Redden <dubajkaro@gmail.com>
2024-01-02 20:12:06 +0100

    merge date picker with async await

--- index.js
+++ index.js
@@ -25,16 +25,11 @@ async function getNasaPictue() {
 getNasaPictue();
 
 //asteroids feed api
-
+//const feedUrl = 'https://api.nasa.gov/neo/rest/v1/feed?start_date=2015-09-07&end_date=2015-09-08&api_key=DEMO_KEY';
 const startInput = document.querySelector('#start');
 console.log(startInput);
 
-async function getAsteroids() {
-  let responseAsteroids = await fetch('https://api.nasa.gov/neo/rest/v1/feed?start_date=2024-01-01&end_date=2024-01-02&api_key=Z6B0Pbqt2FldR7fPdreYIZLBXDaAnZGdKZ4kxicX');
-  //console.log(responseAsteroids);
-  let astroidsResponse = await responseAsteroids.json();
-  console.log(astroidsResponse);
-  //config the date picker
+document.addEventListener('DOMContentLoaded', function () {
   const elems = document.querySelectorAll('.datepicker');
   const instances = M.Datepicker.init(elems, {
     autoClose: true,
@@ -42,10 +37,17 @@ async function getAsteroids() {
     format: 'yyyy-mm-dd'
   });
 
+  startInput.addEventListener('change', function () {
+    const startDateValue = this.value;
+    async function getAsteroids() {
+      let responseAsteroids = await fetch(`https://api.nasa.gov/neo/rest/v1/feed?start_date=${startDateValue}&api_key=${apiKey}`);
+      //console.log(responseAsteroids);
+      let astroidsResponse = await responseAsteroids.json();
+      console.log(astroidsResponse);
+      //config the date picker
+    }
+    console.log(this.value)
+    getAsteroids()
+  });
+});
 
-
-
-
-
-}
-getAsteroids()
